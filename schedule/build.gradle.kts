e3383fd2fe1492a0f15ed027e231716aa374ddb7
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2025-04-13 13:21:43 +0200

    Create schedule module

---
+++ schedule/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.hilt.android)
+}
+
+android {
+    namespace = "com.davidmendozamartinez.schedule"
+    compileSdk = 35
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    api(project(":data"))
+
+    implementation(libs.hilt.android)
+
+    kapt(libs.hilt.android.compiler)
+}
