09ff58219238fc434a0fb7bc4bbf9210bf431a59
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2025-06-30 14:24:42 +0200

    Create SettingsSuccessState

--- feature/settings/src/main/java/com/davidmendozamartinez/sunrating/feature/settings/model/preview/EventAlertSettingsUiStatePreviewBuilder.kt
+++ feature/settings/src/main/java/com/davidmendozamartinez/sunrating/feature/settings/model/preview/EventAlertSettingsUiStatePreviewBuilder.kt
@@ -1,21 +1,19 @@
 package com.davidmendozamartinez.sunrating.feature.settings.model.preview
 
-import com.davidmendozamartinez.sunrating.domain.event.model.Event
 import com.davidmendozamartinez.sunrating.feature.settings.model.AdvanceSettingsUiState
 import com.davidmendozamartinez.sunrating.feature.settings.model.AdvanceUiState
 import com.davidmendozamartinez.sunrating.feature.settings.model.EventAlertSettingsTypeUiState
 import com.davidmendozamartinez.sunrating.feature.settings.model.EventAlertSettingsUiState
+import com.davidmendozamartinez.sunrating.feature.settings.model.QualityThresholdSettingsUiState
 
 fun buildFakeEventAlertSettingsUiState(
     typeUiState: EventAlertSettingsTypeUiState = EventAlertSettingsTypeUiState.SUNRISE,
     isEnabled: Boolean = true,
     advanceSettingsUiState: AdvanceSettingsUiState = AdvanceSettingsUiState(selected = AdvanceUiState.TEN_MINUTES),
-    qualityThreshold: Float = 2.5f,
-    qualityScale: Int = Event.QUALITY_SCALE,
+    qualityThresholdSettingsUiState: QualityThresholdSettingsUiState = QualityThresholdSettingsUiState(value = 2.5f),
 ): EventAlertSettingsUiState = EventAlertSettingsUiState(
     typeUiState = typeUiState,
     isEnabled = isEnabled,
     advanceSettingsUiState = advanceSettingsUiState,
-    qualityThreshold = qualityThreshold,
-    qualityScale = qualityScale,
+    qualityThresholdSettingsUiState = qualityThresholdSettingsUiState,
 )
---
+++ feature/settings/src/main/java/com/davidmendozamartinez/sunrating/feature/settings/state/SettingsSuccessState.kt
@@ -0,0 +1,94 @@
+package com.davidmendozamartinez.sunrating.feature.settings.state
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import com.davidmendozamartinez.sunrating.feature.settings.component.EventAlertSettings
+import com.davidmendozamartinez.sunrating.feature.settings.model.AdvanceUiState
+import com.davidmendozamartinez.sunrating.feature.settings.model.EventAlertSettingsTypeUiState
+import com.davidmendozamartinez.sunrating.feature.settings.model.SettingsUiState
+import com.davidmendozamartinez.sunrating.feature.settings.model.preview.buildFakeEventAlertSettingsUiState
+import com.davidmendozamartinez.sunrating.ui.R
+import com.davidmendozamartinez.sunrating.ui.component.theme.ThemedButton
+import com.davidmendozamartinez.sunrating.ui.designsystem.SunRatingTheme
+import kotlinx.collections.immutable.persistentListOf
+
+@Composable
+internal fun SettingsSuccessState(
+    uiState: SettingsUiState.Success,
+    onEventAlertEnableCheckedChange: (EventAlertSettingsTypeUiState, Boolean) -> Unit,
+    onEventAlertAdvanceItemClick: (EventAlertSettingsTypeUiState, AdvanceUiState) -> Unit,
+    onEventAlertQualityThresholdValueChange: (EventAlertSettingsTypeUiState, Float) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = SunRatingTheme.spacing.space4)
+            .padding(bottom = SunRatingTheme.spacing.space4),
+    ) {
+        LazyColumn(
+            modifier = Modifier.weight(weight = 1f),
+            contentPadding = PaddingValues(top = SunRatingTheme.spacing.space4),
+            verticalArrangement = Arrangement.spacedBy(space = SunRatingTheme.spacing.space16),
+        ) {
+            items(items = uiState.items) { itemUiState ->
+                EventAlertSettings(
+                    uiState = itemUiState,
+                    onEnableCheckedChange = { onEventAlertEnableCheckedChange(itemUiState.typeUiState, it) },
+                    onQualityThresholdValueChange = { onEventAlertQualityThresholdValueChange(itemUiState.typeUiState, it) },
+                )
+            }
+        }
+
+        ThemedButton(
+            text = stringResource(id = R.string.settings_button_save),
+            onClick = onSaveClick,
+            modifier = Modifier
+                .padding(top = SunRatingTheme.spacing.space4)
+                .fillMaxWidth(),
+            enabled = uiState.isSaveButtonEnabled,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun SettingsSuccessStatePreview(
+    @PreviewParameter(provider = SettingsSuccessStatePreviewParameterProvider::class) uiState: SettingsUiState.Success,
+) {
+    SunRatingTheme {
+        SettingsSuccessState(
+            uiState = uiState,
+            onEventAlertEnableCheckedChange = { _, _ -> },
+            onEventAlertAdvanceItemClick = { _, _ -> },
+            onEventAlertQualityThresholdValueChange = { _, _ -> },
+            onSaveClick = {},
+        )
+    }
+}
+
+private class SettingsSuccessStatePreviewParameterProvider : PreviewParameterProvider<SettingsUiState.Success> {
+    override val values: Sequence<SettingsUiState.Success>
+        get() = sequenceOf(
+            SettingsUiState.Success(
+                items = persistentListOf(
+                    buildFakeEventAlertSettingsUiState(typeUiState = EventAlertSettingsTypeUiState.SUNRISE, isEnabled = false),
+                    buildFakeEventAlertSettingsUiState(typeUiState = EventAlertSettingsTypeUiState.SUNSET, isEnabled = true),
+                ),
+                isSaveButtonEnabled = true,
+            ),
+        )
+}
