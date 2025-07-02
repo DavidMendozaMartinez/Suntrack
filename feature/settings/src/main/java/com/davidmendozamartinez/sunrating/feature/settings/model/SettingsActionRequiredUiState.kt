e191cdfbf66e375f19a7942fd5ca751b8681ad8c
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2025-07-02 14:41:02 +0200

    Create SettingsActionRequiredUiState

---
+++ feature/settings/src/main/java/com/davidmendozamartinez/sunrating/feature/settings/model/SettingsActionRequiredUiState.kt
@@ -0,0 +1,39 @@
+package com.davidmendozamartinez.sunrating.feature.settings.model
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Alarm
+import androidx.compose.material.icons.outlined.NotificationsActive
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.res.stringResource
+import com.davidmendozamartinez.sunrating.ui.R
+
+sealed interface SettingsActionRequiredUiState {
+    @get:Composable
+    val iconPainter: Painter
+
+    @get:Composable
+    val message: String
+
+    @get:Composable
+    val action: String
+
+    data object NotificationsPermission : SettingsActionRequiredUiState {
+        override val iconPainter: Painter
+            @Composable get() = rememberVectorPainter(image = Icons.Outlined.NotificationsActive)
+        override val message: String
+            @Composable get() = stringResource(id = R.string.settings_action_required_permission_notifications_message)
+        override val action: String
+            @Composable get() = stringResource(id = R.string.settings_action_required_permission_notifications_action)
+    }
+
+    data object ExactAlarm : SettingsActionRequiredUiState {
+        override val iconPainter: Painter
+            @Composable get() = rememberVectorPainter(image = Icons.Outlined.Alarm)
+        override val message: String
+            @Composable get() = stringResource(id = R.string.settings_action_required_exact_alarm_message)
+        override val action: String
+            @Composable get() = stringResource(id = R.string.settings_action_required_exact_alarm_action)
+    }
+}
